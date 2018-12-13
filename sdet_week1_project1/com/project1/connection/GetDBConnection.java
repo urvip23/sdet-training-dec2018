fe48dc13b42a466468a31b55dbaf58604b46bb0e
Urvi <urvip23@gmail.com>
2018-12-13 10:12:52 -0500

    SDET Project 1
    This is the Account project for SDET Week 1 Training

---
+++ sdet_week1_project1/com/project1/connection/GetDBConnection.java
@@ -0,0 +1,35 @@
+package com.project1.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class GetDBConnection {
+	//connection variables
+	static Connection connection;
+	//ResultSet ps;
+	public PreparedStatement ps,  ps1, ps2;
+	public ResultSet rs, rs1, rs2;
+	
+	public static Connection getMySqlConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection
+					("jdbc:mysql://localhost/sdetdec?autoreconnect=true&useSSL=false", "root", "Hexaware@123");
+			return connection;
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+		
+	}
+	
+	public Connection getOracleConnection() {
+		return null;
+	}
+	
+}
