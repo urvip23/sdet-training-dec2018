1a1aff191ee00277b126293b19bb5a798bf9e000
Urvi <urvip23@gmail.com>
2018-12-17 16:48:39 -0500

    Refactored

--- JavaWorks/src-sdetweek2-automationtesting/com/training/sdet/day1/testng/TestNG_ShoppingCart.java
+++ JavaWorks/src-sdetweek2-automationtesting/com/training/sdet/day1/testng/TestNG_ShoppingCart.java
@@ -1,4 +1,4 @@
-package com.training.sdet.day1.selenium.webdriver;
+package com.training.sdet.day1.testng;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterGroups;
