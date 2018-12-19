8f04343d9968052081532685d7ee8e05b3d75bf8
Urvi <urvip23@gmail.com>
2018-12-19 15:14:34 -0500

    New java files

--- JavaWorks/src-sdetweek2-automationtesting/com/training/sdet/testng/DataProvider01.java
+++ JavaWorks/src-sdetweek2-automationtesting/com/training/sdet/testng/DataProvider01.java
@@ -7,7 +7,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import org.apache.commons.io.FileUtils;
-//import org.apache.log4j.xml.DOMConfigurator;
+import org.apache.log4j.xml.DOMConfigurator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -49,9 +49,9 @@ public class DataProvider01 {
 				//"Chrome";
 		WebDriver driver;
 
-		// DOMConfigurator.configure("log4j.xml");
+		DOMConfigurator.configure("log4j.xml");
 		System.out.println("TestCase Name is: " + TestCaseName);
-		// Log.startTestCase(TestCaseName);
+		Log.startTestCase(TestCaseName);
 
 		
 		extentReport = new ExtentReports("C:\\121718\\Reports\\ExtentReport.html", false);
@@ -72,8 +72,8 @@ public class DataProvider01 {
 		// System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
 		// driver = new FirefoxDriver();
 
-	//	System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-	//	driver = new ChromeDriver();
+		//	System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+	    //	driver = new ChromeDriver();
 
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
@@ -82,7 +82,7 @@ public class DataProvider01 {
 
 		System.out.println("Opening Browser: " + runTestInBrowser);
 		extentTest.log(LogStatus.INFO, "Opening Chrome Browser");
-		// Log.info("Opening Browser: " + runTestInBrowser);
+		 Log.info("Opening Browser: " + runTestInBrowser);
 		/*
 		 * System.setProperty("webdriver.firefox.marionette",
 		 * "C:\\SeleniumJAVATraining\\Softwares\\GeckoDriver\\64bit\\geckodriver.exe");
@@ -93,7 +93,7 @@ public class DataProvider01 {
 		driver.findElement(By.name("txtSearch")).clear();
 		driver.findElement(By.name("txtSearch")).sendKeys(item2Search);
 		extentTest.log(LogStatus.INFO, "Searching Item: " +item2Search);
-		// Log.info("Searching for Item: " + item2Search);
+		Log.info("Searching for Item: " + item2Search);
 		System.out.println("Searching for Item: " + item2Search);
 
 		driver.findElement(By.id("Go")).click();
@@ -102,7 +102,7 @@ public class DataProvider01 {
 		
 		driver.findElement(By.name("txtItemQty1")).clear();
 		driver.findElement(By.name("txtItemQty1")).sendKeys(qty);
-		// Log.info("Qty added to cart: " + qty);
+		 Log.info("Qty added to cart: " + qty);
 		System.out.println("Qty added to cart: " + qty);
 
 		driver.findElement(By.id("Recalc")).click();
@@ -110,19 +110,19 @@ public class DataProvider01 {
 
 		driver.findElement(By.name("txtCustomerName")).clear();
 		driver.findElement(By.name("txtCustomerName")).sendKeys(customerName);
-		// Log.info("customerName: " + customerName);
+		 Log.info("customerName: " + customerName);
 		System.out.println("customerName: " + customerName);
 
 		driver.findElement(By.name("txtAddress")).clear();
 		driver.findElement(By.name("txtAddress")).sendKeys("123 Main Street");
 		driver.findElement(By.name("txtCity")).clear();
 		driver.findElement(By.name("txtCity")).sendKeys(customerCity);
-		// Log.info("customerCity: " + customerCity);
+		 Log.info("customerCity: " + customerCity);
 		System.out.println("customerCity: " + customerCity);
 
 		driver.findElement(By.name("txtState")).clear();
 		driver.findElement(By.name("txtState")).sendKeys(customerState);
-		// Log.info("customerState: " + customerState);
+		 Log.info("customerState: " + customerState);
 
 		driver.findElement(By.name("txtZIP")).clear();
 		driver.findElement(By.name("txtZIP")).sendKeys("20121");
@@ -155,13 +155,13 @@ public class DataProvider01 {
 		driver.findElement(By.xpath("(//input[@name='cmdSubmit'])[3]")).click();
 		String orderConfirmation = driver.findElement(By.cssSelector("h2")).getText();
 		// System.out.println("orderConfirmation: " + orderConfirmation);
-		// Log.info("orderConfirmation: " + orderConfirmation);
+		 Log.info("orderConfirmation: " + orderConfirmation);
 
 		System.out.println("Order Confirmation Message: " + orderConfirmation);
 
 		driver.quit();
 		System.out.println("End executing TestCaseName: " + TestCaseName);
-//		 Log.endTestCase(TestCaseName);
+		 Log.endTestCase(TestCaseName);
 		 extentReport.endTest(extentTest);
 		 extentReport.flush();
 		 extentReport.close();
